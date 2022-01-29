fbcf217d9ccf6873cd5db78542b0bfb2919f6adb
Sourav Kayal <sourav.kayal@yahoo.com>
2022-01-29 21:05:05 +0530

    first commit

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XssValidator.Controllers
+{
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+
+        public PersonController()
+        {
+
+        }
+
+        [HttpPost]
+        public ActionResult Create(Person person)
+        {
+            return Ok();
+        }
+
+    }
+}
